7a2d1a3f259767d34c68de9388c1a16771803e16
PatilDurga <86966918+PatilDurga@users.noreply.github.com>
2021-09-21 14:22:06 +0530

    Day-1 Assignment Submitted

---
+++ simple_interest.py
@@ -0,0 +1,11 @@
+p=int(input("Enter Principle Amount:"))
+r=int(input("Enter Rate:"))
+t=int(input("Enter Time Duration:"))
+
+si=(p*t*r)/100
+
+print("\n")
+print("The principle Amount :",p)
+print("The rate of interest :",r)
+print("The time period is :",t)
+print("Simple Interest is :",si)
